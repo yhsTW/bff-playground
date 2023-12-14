bd493b18c9c69289d3bf8fa513226c70e709e03e
YHS-mac <yhs0823@twinny.co.kr>
2023-12-14 15:12:01 +0900

    쿼리 분리

--- src/services/robot.js
+++ src/services/robot.js
@@ -1,4 +1,5 @@
 const { taras, tarp } = require('../instances');
+const { query } = require('../graphql/robot');
 
 const getRobot = ({
   filter: {
@@ -6,33 +7,9 @@ const getRobot = ({
     workspace: { id },
   },
 }) => {
-  const TARAS_QUERY = `#graphql
-    query GetRobot($key: ID) {
-      robot(filters: {key: $key}) {
-        key
-        name
-      }
-    }
-  `;
-
-  const TARP_QUERY = `#graphql
-    query GetRobots($id: ID) 
-      workspace(filter: {id: $id}) {
-        id
-        name
-        robots {
-          key
-          missionID
-          isAssignable
-          last_connected_time
-        }
-      }
-    }
-  `;
-
   return Promise.all([
-    taras({ query: TARAS_QUERY, variables: { key } }),
-    tarp({ query: TARP_QUERY, variables: { id } }),
+    taras({ query: query.taras.getRobot, variables: { key } }),
+    tarp({ query: query.tarp.getRobotByWorkspaceId, variables: { id } }),
   ]).then((res) => {
     const tarasRobot = res[0].data.robot[0];
     const tarpRobot = res[1].data.workspace[0].robots.filter((robot) => robot.key === key)[0];
